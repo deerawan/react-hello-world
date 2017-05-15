af42a8a28e2a5e08bab5db118ebd7f79ce6c0b49
Budi Irawan <deerawan@gmail.com>
2017-05-15 21:15:31 +1000

    add state

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './HelloWorld.css';
 
-const HelloWorld = (props) => {
-  return (<div className="HelloWorld">Hello world {props.name}</div>);
-};
+class HelloWorld extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {greeting: 'Hello'};
+
+        this.frenchify = this.frenchify.bind(this);
+    }
+    frenchify() {
+        this.setState({greeting: 'Bonjour'});
+    }
+    render() {
+        return (
+            <div className="HelloWorld">
+                {this.state.greeting} {this.props.name}
+                <br />
+                <button onClick={this.frenchify}>frenchify!</button>
+            </div>
+        );
+    }
+}
 
 export default HelloWorld;
